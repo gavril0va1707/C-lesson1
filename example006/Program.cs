63e2b7f08fc385940896ba7d9779a8021d6dc8b9
gavril0va1707 <gavril0va1707@yandex.ru>
2023-10-19 12:30:41 +0300

    Добавили 6  программу

---
+++ example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+
+}
+else
+{
+     Console.WriteLine("Привет, ");
+      Console.WriteLine(username);
+}
